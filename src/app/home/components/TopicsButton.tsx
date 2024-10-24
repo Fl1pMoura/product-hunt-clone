1d7d9bf5813082f17ae971f7d5aa384e781fba05
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-10-23 21:43:28 -0300

    feat: create trending topics buttons hover

--- src/app/home/components/TopicsButton.tsx
+++ src/app/home/components/TopicsButton.tsx
@@ -4,8 +4,10 @@ interface TopicsButtonProps {
 
 export default function TopicsButton({ buttonText }: TopicsButtonProps) {
   return (
-    <li className="cursor-pointer flex items-center justify-center px-3 py-2 font-medium text-font-light-blue capitalize text-sm bg-font-lighter-blue rounded-lg w-fit h-fit">
-      {buttonText}
+    <li>
+      <button className="cursor-pointer flex items-center justify-center px-3 py-2 font-medium text-font-light-blue capitalize text-sm bg-font-lighter-blue rounded-lg w-fit h-fit transition-all duration-300 hover:bg-font-light-orange hover:text-font-black">
+        {buttonText}
+      </button>
     </li>
   );
 }
